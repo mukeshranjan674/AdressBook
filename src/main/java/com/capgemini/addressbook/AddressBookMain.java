0331db0948b1f599492693395304dc75bae03221
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-19 10:35:42 +0530

    [Mukesh] add : Ability to create a contact

--- src/main/java/com/capgemini/addressbook/AddressBookMain.java
+++ src/main/java/com/capgemini/addressbook/AddressBookMain.java
@@ -3,5 +3,7 @@ package com.capgemini.addressbook;
 public class AddressBookMain {
 public static void main(String[] args) {
 	System.out.println("***** Welcom to Address Book Program *****");
+	ContactPerson c = new ContactPerson("Mukesh" , "Ranjan" , "Sijua" , "Dhanbad" , "Jharkhand" , "828108" , "1111111111" , "abc@gmail.com");
+	System.out.println(c);
 }
 }
