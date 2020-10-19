33427a020072d9bed8b3e17b82becbb51fc86b68
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-19 10:32:45 +0530

    [Mukesh] add : AddressBookMain class written

---
+++ src/main/java/com/capgemini/addressbook/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.capgemini.addressbook;
+
+public class AddressBookMain {
+public static void main(String[] args) {
+	System.out.println("***** Welcom to Address Book Program *****");
+}
+}
